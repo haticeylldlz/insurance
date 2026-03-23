f5bac92e3658c49d9a54c5ff4476a17a6a7c0efb
Hatice Yildiz <hylldlz2131@gmail.com>
2026-03-23 22:59:12 +0200

    Fix auth quotes and enable login/register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,31 +2,53 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insurance App</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">Insurance App</a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('owners.index') }}">Owners</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cars.index') }}">Cars</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 
+<nav class="navbar navbar-dark bg-dark mb-4">
     <div class="container">
-        @yield('content')
-    </div>
+        <a class="navbar-brand" href="{{ url('/') }}">Insurance App</a>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        <ul class="navbar-nav flex-row">
+
+            <li class="nav-item me-3">
+                <a class="nav-link text-white" href="{{ route('owners.index') }}">Owners</a>
+            </li>
+
+            <li class="nav-item me-3">
+                <a class="nav-link text-white" href="{{ route('cars.index') }}">Cars</a>
+            </li>
+
+            @guest
+                <li class="nav-item me-3">
+                    <a class="nav-link text-white" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ route('register') }}">Register</a>
+                </li>
+            @else
+                <li class="nav-item me-3 text-white">
+                    {{ auth()->user()->name }}
+                </li>
+
+                <li class="nav-item">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button class="btn btn-link text-white p-0">Logout</button>
+                    </form>
+                </li>
+            @endguest
+
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
+    @yield('content')
+</div>
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
+
+Route::get('/', function () {
+    return redirect()->route('owners.index');
+});
+
 use App\Http\Controllers\OwnerController;
 use App\Http\Controllers\CarController;
 
-Route::resource('owners', OwnerController::class);
-Route::resource('cars', CarController::class);
-
-Route::get('/', function () {
-    return redirect()->route('owners.index'); // Ana sayfa Owner index
+Route::middleware('auth')->group(function () {
+    Route::resource('owners', OwnerController::class);
+    Route::resource('cars', CarController::class);
 });
